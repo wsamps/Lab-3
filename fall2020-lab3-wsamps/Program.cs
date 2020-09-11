952f3b7312c03f380377d20ab1177cc22386b509
wsamps <70773581+wsamps@users.noreply.github.com>
2020-09-11 13:38:48 -0500

    Add files via upload

---
+++ fall2020-lab3-wsamps/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _221StarterCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+           
+ //           Random randomNumber = new Random();
+   //         int number;
+     //       number = randomNumber.Next(1, 4);
+        }
+    }
+}
